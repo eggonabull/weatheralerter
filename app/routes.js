f6a32b6e77eea46c5c225e11d5fe0c50dae074e4
Drew Faubion <eggonabull@hotmail.com>
2017-01-14 23:36:26 -0800

    Start AWS SNS integration.

--- app/routes.js
+++ app/routes.js
@@ -7,6 +7,38 @@ var Wunderground = require('wundergroundnode');
 console.log(Secrets.wundergroundKey())
 var wunderground = new Wunderground(Secrets.wundergroundKey())
 
+var AWS = require('aws-sdk');
+
+var s3 = new AWS.S3();
+
+
+var sns = new AWS.SNS();
+sns.addPermission(params, function (err, data) {
+  if (err) console.log(err, err.stack); // an error occurred
+  else     console.log(data);           // successful response
+});
+
+
+var params = {
+  Message: 'STRING_VALUE', /* required */
+  MessageAttributes: {
+    someKey: {
+      DataType: 'STRING_VALUE', /* required */
+      BinaryValue: new Buffer('...') || 'STRING_VALUE',
+      StringValue: 'STRING_VALUE'
+    },
+    /* anotherKey: ... */
+  },
+  MessageStructure: 'STRING_VALUE',
+  PhoneNumber: 'STRING_VALUE',
+  Subject: 'STRING_VALUE',
+  TargetArn: 'STRING_VALUE',
+  TopicArn: 'STRING_VALUE'
+};
+sns.publish(params, function(err, data) {
+  if (err) console.log(err, err.stack); // an error occurred
+  else     console.log(data);           // successful response
+});
 
 module.exports = function(app) {
     // server routes ===========================================================

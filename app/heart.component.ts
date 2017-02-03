04015c7962221aec374912fe73f3e064a0cfa786
Drew Faubion <eggonabull@hotmail.com>
2017-02-03 23:57:00 +0800

    Get a real start on the heart component.

--- app/heart.component.ts
+++ app/heart.component.ts
@@ -1,1 +1,27 @@
-heart.component.ts
+import {Component} from 'angular2/core'
+import {Input, Output} from 'angular2/core'
+import {EventEmitter} from 'angular2/core'
+
+@Component({
+	selector: 'heart',
+	template: `
+		<i class="glyphicon-heart"></i>
+		<span class="count">{{click}}</span>
+	`,
+	styles: [`
+		.glyphicon-star {
+			color: orange;
+		}
+	`]
+})
+export class StarComponent {
+	@Input('is-heart') isFavorite : boolean = true;
+
+	@Output() change = new EventEmitter();
+
+	onToggle() {
+		this.isFavorite = !this.isFavorite;
+		this.change.emit({ newValue: this.isFavorite });
+	}
+
+}
